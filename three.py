c6f5051fb22484ee40deb563d07bc4399dc29952
Riboloff <makcimgovorov@gmail.com>
2019-02-20 20:36:32 +0300

    3.2

--- three.py
+++ three.py
@@ -49,7 +49,56 @@ class MultiStack:
             self.array.pop()
             self.array.pop()
 
+class NodeStack:
+    def __init__(self, value=None):
+        self.value = value
+        self.pre_min = None
+        self.current_min = 0
 
+class SimpleStack:
+    def __init__(self):
+        self.array = []
+        self.min = None
+
+    def push(self, value):
+        if (value < 0):
+            return
+        node = NodeStack(value)
+        self.array.append(node)
+        if (self.min == None):
+            self.min = value
+            node.pre_min = None
+
+        else:
+            if (self.min >= value):
+                node.pre_min = self.min
+                node.current_min = 1
+                self.min = value
+
+    def pop(self):
+        if len(self.array):
+            node = self.array.pop()
+            if (node.current_min == 1):
+                self.min = node.pre_min
+            if (len(self.array) == 0):
+                self.min = None
+
+            return node.value
+
+        self.min = None
+        return None
+
+    def out(self):
+        ar = []
+        for node in self.array:
+            ar.append(node.value)
+        print ar
+
+    def _min(self):
+        return self.min
+
+
+print '===========3.1.MultiStack========'
 ms = MultiStack(2)
 ms.add(0, 'iddqd')
 ms.add(1, 'idkfa')
@@ -73,4 +122,41 @@ ms.add(0, 'idclip')
 ms.add(0, 'iddt')
 ms.add(1, 'iddt')
 ms.out()
+ms.out()
 
+
+print '===========3.2.midle========'
+s = SimpleStack()
+s.push(1)
+s.push(2)
+s.push(3)
+s.push(1)
+s.push(2)
+s.out()
+print "min=", s._min()
+s.pop()
+s.out()
+print "min=", s._min()
+s.pop()
+s.out()
+print "min=", s._min()
+s.pop()
+s.out()
+print "min=", s._min()
+s.pop()
+s.out()
+print "min=", s._min()
+s.pop()
+s.out()
+print "min=", s._min()
+s.push(2)
+s.out()
+s.push(3)
+s.push(0)
+s.push(0)
+s.push(0)
+s.push(3)
+s.push(4)
+s.push(3)
+s.out()
+print "min=", s._min()
